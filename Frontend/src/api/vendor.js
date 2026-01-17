19ee2bb6ea75f46a8bad90b72e9a516297c0991d
kuruet <bpratik728@gmail.com>
2026-01-17 23:29:04 +0530

    fix: use env-based backend API URL

--- Frontend/src/api/vendor.js
+++ Frontend/src/api/vendor.js
@@ -2,8 +2,9 @@ import axios from "axios";
 import { logoutVendor } from "../utils/auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
+
 
 api.interceptors.response.use(
   (res) => res,
